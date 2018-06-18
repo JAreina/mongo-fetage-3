3df3d6c8812b324faddb7e32e3d0c30944b740d5
JAreina <jareinafdez@gmail.com>
2018-06-18 12:43:34 +0200

    promesas

--- 2-crear-coleccion/2-crear-coleccion.js
+++ 2-crear-coleccion/2-crear-coleccion.js
@@ -1,25 +1,23 @@
 const mongo = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017";
+const url = 'mongodb://localhost:27017';
 
-// mogodb funciones asincoranas 
-
+// mogodb funciones asincoranas
 
 //CONEXION CON  callbacks
 
-const dbName = "bbdd"
+const dbName = 'bbdd';
 
-mongo.connect(url, function(err,client){
-    
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log("connectado a base de datos 1: "+client.s.options.dbName);
+mongo.connect(url, function(err, client) {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log('connectado a base de datos 1: ' + client.s.options.dbName);
 
-    const  base = client.db(dbName);
-    let bicis =  base.collection('bicicletas');
+	const base = client.db(dbName);
+	let bicis = base.collection('bicicletas');
 
-/*
+	/*
                    // CREAR COLECCION               
         base.createCollection("bicicletas", 
                   { "capped": true, "size": 100000, "max": 5000},
@@ -44,34 +42,41 @@ mongo.connect(url, function(err,client){
          })
 
 */
-                           // UPDATE ONE 
-        bicis.updateOne({"d":24}, {$set: {b:888888888}},(err,r)=>{
-            console.log("UPDATE ONE ")
-        })
+	// UPDATE ONE
+	bicis.updateOne({ d: 24 }, { $set: { b: 888888888 } }, (err, r) => {
+		console.log('UPDATE ONE ');
+	});
 
-        bicis.findOne({"d":24},(err,r)=>{
-            if(err){
-                console.log(err);
-                return;
-            }
-                console.log(r);
-        })
-    
+	bicis.findOne({ d: 24 }, (err, r) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log(r);
+		client.close();
+		segundaConexion();
+	});
 });
 //CONEXION CON promesas
 
-/*
-let conexion = mongo.connect(url);
+function segundaConexion() {
+	let conexion = mongo.connect(url);
 
-console.log(conexion);
+	console.log(conexion);
 
-conexion
-.then(
-    (db)=>{
-        console.log("connectado a base de datos 2: "+db.s.options.dbName);
-        db.close();
-    }
-)
-.catch((err)=>{
-    
-});*/
+	conexion
+		.then((db) => {
+			console.log('connectado a base de datos 2: ' + db.s.options.dbName);
+			const base = db.db(dbName);
+			let bicis = base.collection('bicicletas');
+
+            return bicis.findOne({ c: 55 });
+            //lo devuelve al siguiente then(datos)
+		})
+		.then((datos) => {
+            console.log(datos);
+            return datos;
+            
+		}).then(datos => console.log(datos))
+		.catch((err) => {});
+}
