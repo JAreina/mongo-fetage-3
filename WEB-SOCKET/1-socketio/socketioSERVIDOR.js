6af1b8932ffd6832fee736c4da32d996cb41a2e2
JAreina <jareinafdez@gmail.com>
2018-07-04 12:03:08 +0200

    socket

--- WEB-SOCKET/1-socketio/socketioSERVIDOR.js
+++ WEB-SOCKET/1-socketio/socketioSERVIDOR.js
@@ -6,5 +6,19 @@ let app = express();
 let server = http.createServer(app);
 let io = socketio(server);
 
+app.use(express.static("./recursosestaticos"))
 
-server.listen(23999, ()=>console.log("espero conexiones"));
+server.listen(23999, ()=>
+  console.log("espero conexiones http://localhost:23999"));
+
+
+  io.on("connection",(socket)=>{
+        console.log("cliente conectado::::::: "+ socket.id);
+         
+        socket.on('identificador', identificador =>{
+            console.log(socket.id +" ............."+identificador)
+        })
+
+  });
+
+ 
